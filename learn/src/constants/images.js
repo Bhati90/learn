5b763b2a1a293431ece43dc886cb37083dbb553b
kishan bhati <bhatikaran634@gmail.com>
2023-06-15 18:16:22 +0530

    first

---
+++ learn/src/constants/images.js
@@ -0,0 +1,26 @@
+import logo from '../assets/logo.png';
+import cart1 from '../assets/cart1.png';
+import cart2 from '../assets/cart2.png';
+import cart3 from '../assets/cart3.png';
+import cart4 from '../assets/cart4.png';
+import cart5 from '../assets/cart5.png';
+import cart6 from '../assets/cart6.png';
+import headl from '../assets/headl.png';
+import jass from '../assets/jass.png';
+import foot1 from '../assets/foot1.png';
+
+
+export default {
+    logo,
+    cart1,
+    cart2,
+    cart3,
+    cart4,
+    cart5,
+    cart6,
+    headl,
+    jass,
+    foot1
+};
+
+// export default images
