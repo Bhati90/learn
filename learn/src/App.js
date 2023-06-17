57838f2f5db6c06abadbd4a1756ed4bd3655c858
kishan bhati <bhatikaran634@gmail.com>
2023-06-18 01:18:32 +0530

    videoplayer

--- learn/src/App.js
+++ learn/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar/Navbar';
 import Header from './container/Header/Header';
 import Learn from './container/Learn/Learn';
 import Mid from './container/Mid/Mid';
+// import Video from './container/Video/Video/Video';
+import VideoMain from './container/Video/Video/VideoMain';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Header/>
       <Mid/>
       <Learn/>
+      <VideoMain/>
     </div>
   );
 }
